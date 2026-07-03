aca647314a9c014d8a4585d2a50438e0c29b11fe
youssefelmoumni <45952387+youssefelmoumni@users.noreply.github.com>
2026-07-03 14:59:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,13 @@
-document.getElementById("matches").innerHTML=`
-<p>Argentina vs Brazil</p>
-<p>Today - 20:00 UTC</p>
-`;
+fetch("https://elmoumniyoussef-ff.workers.dev")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
 
-document.getElementById("news").innerHTML=`
-<p>Welcome to World Cup Live.</p>
-<p>Football news will appear here.</p>
-`;
-.bottom-nav{
-position:fixed;
-bottom:0;
-left:0;
-width:100%;
-background:#1e293b;
-display:flex;
-justify-content:space-around;
-padding:10px 0;
-border-top:1px solid #334155;
-}
-
-.bottom-nav a{
-color:white;
-text-decoration:none;
-font-size:13px;
-text-align:center;
-}
-
-body{
-padding-bottom:80px;
-}
+    document.getElementById("matches").innerHTML =
+      "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+  })
+  .catch(error => {
+    console.error(error);
+    document.getElementById("matches").innerHTML =
+      "Unable to load data.";
+  });
