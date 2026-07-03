d763faddf399492a4a90581cbf672bb43c363341
youssefelmoumni <45952387+youssefelmoumni@users.noreply.github.com>
2026-07-03 13:29:10 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("matches").innerHTML=`
+<p>Argentina vs Brazil</p>
+<p>Today - 20:00 UTC</p>
+`;
+
+document.getElementById("news").innerHTML=`
+<p>Welcome to World Cup Live.</p>
+<p>Football news will appear here.</p>
+`;
